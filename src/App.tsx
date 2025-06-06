680d4b19d3099ea8f5ed1716f99aa9553a3a243f
Satra Shufi <90124621@unitedtractors.com>
2025-06-06 15:15:20 +0700

    ✨ : Adding Shadcn and Nice modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { QueryClientProvider } from "@tanstack/react-query"
-import Router, { queryClient } from "./Router"
-import {ReactQueryDevtools} from '@tanstack/react-query-devtools' // Create a client
-import './index.css'
+import { QueryClientProvider } from "@tanstack/react-query";
+import Router, { queryClient } from "./Router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; // Create a client
+import "./index.css";
+import NiceModal from "@ebay/nice-modal-react";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <NiceModal.Provider>
+        <Router />
+      </NiceModal.Provider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
